37ab5777d561dfc74a1ca392c2800cc675265441
Sebastien Bovo <sebastienbovo@outlook.com>
2018-05-18 00:49:22 +0200

    Add the CreateObjectsWithMRTK Unity project

---
+++ CreateMixedRealityAppFrom0/CreateObjectsWithMRTK/Assets/Tap.cs
@@ -0,0 +1,26 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tap : MonoBehaviour, IInputHandler
+{
+    void IInputHandler.OnInputDown(InputEventData eventData)
+    {    }
+
+    void IInputHandler.OnInputUp(InputEventData eventData)
+    {
+        var o = GameObject.CreatePrimitive(PrimitiveType.Cube); 
+        o.transform.localScale = new Vector3(1f, 0.1f, 1f); 
+        o.transform.position = new Vector3(0f, 2f, 4f); 
+        o.AddComponent<Rigidbody>(); 
+    }
+
+    // Use this for initialization
+    void Start()
+    {    }
+
+    // Update is called once per frame
+    void Update()
+    {    }
+}
