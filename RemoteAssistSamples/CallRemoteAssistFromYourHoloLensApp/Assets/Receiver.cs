0b2721af1c82e879f96a86f40a94f06c77de8998
Sebastien Bovo <sebastienbovo@outlook.com>
2018-11-16 18:19:22 +0100

    Fix UI Thread issue + Update 2017.4.15f1

--- RemoteAssistSamples/CallRemoteAssistFromYourHoloLensApp/Assets/Receiver.cs
+++ RemoteAssistSamples/CallRemoteAssistFromYourHoloLensApp/Assets/Receiver.cs
@@ -16,17 +16,21 @@ public class Receiver : InteractionReceiver
     protected override async void InputUp(GameObject obj, InputEventData eventData)
     {
         Debug.Log("InputUp for: " + obj.name);
-
+#if ENABLE_WINMD_SUPPORT
         if (obj.name.CompareTo("RemoteAssistvideoCall") == 0)
         {
             string uriToLaunch = @"ms-voip-video:?contactids=bf538576-8ada-46d8-83ff-1d548b9e7010";
             Debug.Log("LaunchUriAsync: " + uriToLaunch);
 
             var uri = new Uri(uriToLaunch);
-            await LaunchURI(uri);
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                // Work done on the UI thread
+                LaunchURI(uri).ConfigureAwait(false).GetAwaiter().GetResult();
+            });
         }
-
-    }
+#endif
+}
 
     private async Task LaunchURI(System.Uri uri)
     {
--- RemoteAssistSamples/Readme.md
+++ RemoteAssistSamples/Readme.md
@@ -13,3 +13,7 @@ The sample provide you a Holographic button that is always
 - Visible and not directly in the center of the field of view: it seeks to have a part of itself in the view frustum of the camera
 
 The purpose of this sample is to provide you an out of the box tagalong billboard for you apps 🎁!
+
+> **Note:** The code was updated to support Unity 2017.4.15f1
+>
+> **Note:** Keep the Unity folder/project as close as possible to the root of the disk like C:\_Dev\MyApp in order to prevent Unity or Visual studio loading issues with very long paths
